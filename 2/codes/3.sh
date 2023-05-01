7e88f7681f66cf908e40259f7811e0fcf74816c6
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2023-05-01 18:50:50 +0330

    Nearly there...

--- 2/codes/3.sh
+++ 2/codes/3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # CPU
-OUT=$(sudo perf stat -e tlb:tlb_flush,page-faults,dtlb_load_misses.miss_causes_a_walk timeout 30s stress --cpu 10 2>&1)
+OUT=$(sudo perf stat -e tlb:tlb_flush,page-faults,dtlb_load_misses.miss_causes_a_walk stress --cpu 10 --timeout 30 2>&1)
 echo "$OUT"
 tlb=$(echo "$OUT" | grep tlb:tlb_flush | xargs | cut -d ' ' -f1 | tr -d ,)
 pagefault=$(echo "$OUT" | grep page-faults | xargs | cut -d ' ' -f1 | tr -d ,)
@@ -9,7 +9,7 @@ echo "tlb_shootdown,tlb_miss,pagefault" > 3-cpu.csv
 echo "$tlb,$tlbmiss,$pagefault" >> 3-cpu.csv
 sleep 5
 # VM
-OUT=$(sudo perf stat -e tlb:tlb_flush,page-faults,dtlb_load_misses.miss_causes_a_walk timeout 30s stress --vm 4 --vm-bytes 512MB 2>&1)
+OUT=$(sudo perf stat -e tlb:tlb_flush,page-faults,dtlb_load_misses.miss_causes_a_walk stress --vm 4 --vm-bytes 512MB --timeout 30 2>&1)
 echo "$OUT"
 tlb=$(echo "$OUT" | grep tlb:tlb_flush | xargs | cut -d ' ' -f1 | tr -d ,)
 pagefault=$(echo "$OUT" | grep page-faults | xargs | cut -d ' ' -f1 | tr -d ,)
@@ -18,7 +18,7 @@ echo "tlb_shootdown,tlb_miss,pagefault" > 3-vm.csv
 echo "$tlb,$tlbmiss,$pagefault" >> 3-vm.csv
 sleep 5
 # IO
-OUT=$(sudo perf stat -e tlb:tlb_flush,page-faults,dtlb_load_misses.miss_causes_a_walk timeout 30s stress --io 5 2>&1)
+OUT=$(sudo perf stat -e tlb:tlb_flush,page-faults,dtlb_load_misses.miss_causes_a_walk stress --io --timeout 30 2>&1)
 echo "$OUT"
 tlb=$(echo "$OUT" | grep tlb:tlb_flush | xargs | cut -d ' ' -f1 | tr -d ,)
 pagefault=$(echo "$OUT" | grep page-faults | xargs | cut -d ' ' -f1 | tr -d ,)
