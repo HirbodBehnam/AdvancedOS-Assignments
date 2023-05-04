b56d58040604c64e63dd3307fa3683a8b6b41e51
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2023-05-04 14:56:58 +0330

    Finalize

--- 2/codes/1-runner.sh
+++ 2/codes/1-runner.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-echo "threads,tlb,pagefault,time" > stat.csv # empty the stat file
+echo "threads,tlb-shootdown,tlb-miss,pagefault,time" > stat.csv # empty the stat file
 for i in $(seq 1 7); do
-    OUT=$(sudo perf stat -e tlb:tlb_flush,page-faults ./1.out "$i" 2>&1)
+    OUT=$(sudo perf stat -e tlb:tlb_flush,page-faults,dtlb_load_misses.miss_causes_a_walk ./1.out "$i" 2>&1)
     echo "$OUT"
     # In nultshell: Find the line, trim whitespaces, select first column and remove ',' from number
-    itlb=$(echo "$OUT" | grep tlb:tlb_flush | xargs | cut -d ' ' -f1 | tr -d ,)
+    tlb=$(echo "$OUT" | grep tlb:tlb_flush | xargs | cut -d ' ' -f1 | tr -d ,)
     pagefault=$(echo "$OUT" | grep page-faults | xargs | cut -d ' ' -f1 | tr -d ,)
+    tlbmiss=$(echo "$OUT" | grep "dtlb_load_misses.miss_causes_a_walk" | xargs | cut -d ' ' -f1 | tr -d ,)
     time_took=$(echo "$OUT" | grep "seconds time elapsed" | xargs | cut -d ' ' -f1)
-    echo "$i,$itlb,$pagefault,$time_took" >> stat.csv
+    echo "$i,$tlb,$tlbmiss,$pagefault,$time_took" >> stat.csv
 done
